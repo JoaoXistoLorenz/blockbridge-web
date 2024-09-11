17229b7ffae35efeb60c3473057536e8bd1538ad
joao.vitor <joao.vitor@tn3.com.br>
2024-09-10 21:31:29 -0300

    feat: teste6

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
-const fs = require('fs')
+/* const fs = require('fs')
 
-/* VUE_APP_BASE_URL=https://192.168.1.9:8080/
-VUE_APP_BASE_API=https://192.168.1.9:3000 */
+//VUE_APP_BASE_URL=https://192.168.1.9:8080/
+//VUE_APP_BASE_API=https://192.168.1.9:3000
 
 module.exports = {
     devServer: {
-        /* https: {
+        https: {
           key: fs.readFileSync('./cert/certificate.key'),
           cert: fs.readFileSync('./cert/certificate.crt'),
-        }, */
+        },
         public: `${process.env.VUE_APP_BASE_URL}`
     }
-}
+} */
